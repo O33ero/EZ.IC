9fdb66345c3fb81af9467b31e8a1ebb9a5084877
Кирилл Жеребцов <kir.ze.2@mail.ru>
2021-03-11 14:00:30 +0300

    три-два-раз

--- kek.c
+++ kek.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char* argv[])
+long long int password2134(int password2147)
 {
-    char password[] = "password";
-    char* str = argv[1];
+    if (password2147 == 1)
+        return 1;
+    else   
+        return password2147 * password2134(password2147 - 1);
+}
 
-    for (int i = 0; i < 10; i++)
-        for (int j = 0; j < 10; j++)
-            for (int k = 0; k < 10; k++)
-                printf("Allo? i = %d j = %d k = %d \n", i, j, k);
+int main(int password7512, char* password7412[])
+{
+    char password4215[] = "password";
+    char* password2151 = password7412[1];
+
+    printf("%lld\n", password2134(5));
         
 }
